b4f4dee081710876e5377d74b0591352221acabb
Felix Nithammer <f.nithammer@mammutmedia.eu>
2024-06-09 23:36:08 +0200

    fix live stream

--- components/home/LiveStream.vue
+++ components/home/LiveStream.vue
@@ -1,13 +1,17 @@
 <template>
   <v-row class="px-8 px-md-16 py-16 max-width">
     <v-col cols="12" class="text-center">
-      <iframe
-        src="https://player.twitch.tv/?jennysview&parent=ar-homepage.vercel.app&parent=anastasia-rose.de&parent=ar-homepage-git-master-mammut-media.vercel.app&autoplay=true&muted=true"
-        width="960px"
-        height="540px"
-        allowfullscreen
-      >
-      </iframe>
+      <ClientOnly>
+        <iframe
+          src="https://player.twitch.tv/?channel=jennysview&parent=ar-homepage.vercel.app&parent=anastasia-rose.de&parent=ar-homepage-git-master-mammut-media.vercel.app&autoplay=true&muted=true"
+          max-width="960px"
+          width="100%"
+          height="auto"
+          aspect-ratio="16/9"
+          allowfullscreen
+        >
+        </iframe>
+      </ClientOnly>
     </v-col>
   </v-row>
 </template>
